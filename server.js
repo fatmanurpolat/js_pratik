1bd533a0ea52fbe705ce16e6fa3305840a90df0b
Fatmanur Polat <82556567+fatmanurpolat@users.noreply.github.com>
2022-09-23 01:12:59 +0300

    Summary

---
+++ server.js
@@ -0,0 +1,14 @@
+
+
+
+let title = document.getElementById("title")
+title.innerHTML = "Değişen bilgi"
+console.log(title.innerHTML)
+
+
+let link = document.querySelector("#kodluyoruzLink")
+link.innerHTML += ' değişti'
+link.style.color = "purple"
+link.classList.add("btn")
+
+
